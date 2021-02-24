ab32285c57094cc9d712d45c275290ad76c7630e
Thomas Herzog <Tomrule007@gmail.com>
2021-02-24 09:10:59 -0800

    solves js2 problem 2

--- js2/2.js
+++ js2/2.js
@@ -3,15 +3,14 @@
  *   Takes in a function and returns an array.
  *
  * As long as the input function returns false,
- *   array keeps growing with the correspending index value
+ *   array keeps growing with the corresponding index value
  * @param {function} fun
  * @returns {array}
  */
 
-const solution = (fun) => {
-  return []
-}
+const solution = (fun, i = 0) =>
+  fun(i) ? [] : [i].concat(solution(fun, i + 1));
 
 module.exports = {
-  solution
-}
+  solution,
+};
