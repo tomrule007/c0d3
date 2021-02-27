7ee0d57a65a74cb080642bf6efd592dbfda7a18c
Thomas Herzog <Tomrule007@gmail.com>
2021-02-26 23:24:46 -0800

    solves js3/p1

--- js3/1.js
+++ js3/1.js
@@ -4,10 +4,12 @@
  * @returns {array} arr
  */
 
-const solution = (arr, obj) => {
-  return []
-}
+const solution = (arr, obj) =>
+  arr.reduce(
+    (acc, curKey) => (obj.hasOwnProperty(curKey) ? [...acc, obj[curKey]] : acc),
+    []
+  );
 
 module.exports = {
-  solution
-}
+  solution,
+};
