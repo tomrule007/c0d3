5f7a856143a054c3985e5063e48bedd09710840f
Thomas Herzog <Tomrule007@gmail.com>
2021-02-28 00:02:16 -0800

    solves js3/p7

--- js3/7.js
+++ js3/7.js
@@ -2,14 +2,14 @@
  * write a map function for objects
  * @param {callback} cb
  * @returns {number}
-*/
+ */
 
 const solution = () => {
   Object.prototype.map = function (cb) {
-    return []
-  }
-}
+    return Object.entries(this).map(([key, value], i) => cb(key, value, i));
+  };
+};
 
 module.exports = {
-  solution
-}
+  solution,
+};
