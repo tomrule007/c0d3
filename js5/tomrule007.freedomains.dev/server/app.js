cfad28715bcb92f7e2297dc4a42e05d3b1786df2
Thomas Herzog <Tomrule007@gmail.com>
2021-05-27 23:44:06 -0700

    Extend body limit to 5mb

--- js5/tomrule007.freedomains.dev/server/app.js
+++ js5/tomrule007.freedomains.dev/server/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
 
 // *Must require statements must remain wrapped in arrow function to insure files get reloaded
 app.use((req, res, next) => {
