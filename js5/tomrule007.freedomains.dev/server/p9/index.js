ea08f9b83f17c15482f8f56fa8c87f48b57cdc71
Thomas Herzog <Tomrule007@gmail.com>
2021-05-30 20:48:13 -0700

    js5/p9 WIP frontend displays user meme images

--- js5/tomrule007.freedomains.dev/server/p9/index.js
+++ js5/tomrule007.freedomains.dev/server/p9/index.js
@@ -1,6 +1,19 @@
 const { v4: uuidv4 } = require('uuid');
 const router = require('express').Router();
 const cookieParser = require('cookie-parser');
+const fs = require('fs');
+const md5 = require('md5');
+const { version } = require('os');
+const MEME_INDEX_PATH = './data/p9/memeIndex';
+const USER_MEME_PATH = './public/p9/meme/';
+const USER_MEME_URL = '/p9/meme/';
+
+// Create directory if it doesn't exist
+fs.mkdirSync(USER_MEME_PATH, { recursive: true });
+
+// TODO: persist version index;
+const memeVersionIndex = JSON.parse(fs.readFileSync(MEME_INDEX_PATH));
+console.log(memeVersionIndex);
 
 const users = (() => {
   const userByUsername = {};
@@ -27,6 +40,26 @@ const users = (() => {
 })();
 
 router.use(cookieParser());
+
+const protectedRoute = (req, res, next) => {
+  const { loginCookie } = req.cookies;
+  console.log({ loginCookie });
+  if (!loginCookie)
+    res.status(400).json({
+      error: { message: 'Must send login cookie (loginCookie)' },
+    });
+  const user = users.getById(loginCookie);
+  if (!user)
+    res.status(400).json({
+      error: { message: 'Invalid loginCookie' },
+    });
+
+  console.log({ user });
+  req.user = user;
+
+  next();
+};
+
 router.post('/api/user', (req, res) => {
   const { username } = req.body;
   if (!username)
@@ -45,20 +78,60 @@ router.post('/api/user', (req, res) => {
   res.status(201).json({ username });
 });
 
-router.get('/api/user', (req, res) => {
-  const { loginCookie } = req.cookies;
-  if (!loginCookie)
-    res.status(400).json({
-      error: { message: 'Must send login cookie (loginCookie)' },
-    });
-  const user = users.getById(loginCookie);
-  if (!user)
-    res.status(400).json({
-      error: { message: 'Invalid loginCookie' },
-    });
+router.get('/api/user', protectedRoute, (req, res) => {
+  res.cookie('loginCookie', req.user.id, { maxAge: 1000 * 60 * 60 * 24 * 3 }); // 3 days
+  res.status(200).json({ username: req.user.username });
+});
 
-  res.cookie('loginCookie', user.id, { maxAge: 1000 * 60 * 60 * 24 * 3 }); // 3 days
-  res.status(200).json({ username: user.username });
+router.post('/api/meme', protectedRoute, async (req, res) => {
+  console.log('POST');
+  const { selfie, meme } = req.body;
+  if (!selfie)
+    return res
+      .status(400)
+      .json({ error: { message: "Missing 'selfie' field" } });
+
+  const { username } = req.user;
+
+  const memeFileName = username + '.png';
+  const version = md5(selfie + meme);
+
+  // skip update if meme is the same
+  if (!memeVersionIndex[username] || memeVersionIndex[username] !== version) {
+    // save new meme
+    await fs.promises
+      .writeFile(
+        USER_MEME_PATH + memeFileName,
+        selfie.split(',').pop(), //Insures the user didn't include meta data
+        'base64'
+      )
+      .catch((e) => {
+        console.log('WRITE_MEME_ERROR', e);
+      });
+
+    // update version index
+    memeVersionIndex[username] = version;
+
+    // persist updated version (only loaded on server crash/reboot, no need to wait)
+    fs.promises
+      .writeFile(MEME_INDEX_PATH, JSON.stringify(memeVersionIndex))
+      .catch((e) => console.log('MEME_INDEX_PERSIST_ERROR', e));
+  }
+
+  return res
+    .status(200)
+    .json({ link: '/p8/selfie/' + username + '?' + version });
+});
+
+router.get('/api/meme', async (req, res) => {
+  console.log('GET HERER');
+  const memeFilePaths = Object.entries(memeVersionIndex).map(
+    ([username, version]) => ({
+      url: USER_MEME_URL + username + '.png?' + version,
+      username,
+    })
+  );
+  res.status(200).json({ memes: memeFilePaths });
 });
 
 module.exports = router;
