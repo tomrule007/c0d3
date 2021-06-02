132427d04c6d67df6aca63a5721bcb0d89ccd002
Thomas Herzog <Tomrule007@gmail.com>
2021-06-01 17:52:48 -0700

    refactor js5/p6, full async/await & errMsg

--- js5/tomrule007.freedomains.dev/server/p6/index.js
+++ js5/tomrule007.freedomains.dev/server/p6/index.js
@@ -27,68 +27,67 @@ const getUserByKeyValue = (keyValue) =>
     return user[key] === value;
   });
 
+const errMsg = (message) => ({
+  error: { message },
+});
+
 router.post('/api/users', async (req, res) => {
   const { password, username, email, ...extras } = req.body;
   if (!password || password.length <= 5)
-    return res.status(400).json({
-      error: {
-        message:
-          'password field cannot be empty and must be base 64 encoded with more than 5 characters',
-      },
-    });
+    return res
+      .status(400)
+      .json(
+        errMsg(
+          'password field cannot be empty and must be base 64 encoded with more than 5 characters'
+        )
+      );
 
   const alphaNumericOnly = /^[a-z0-9]+$/i;
   if (!username || !alphaNumericOnly.test(username))
-    return res.status(400).json({
-      error: {
-        message:
-          'username field cannot be blank and must contain alpha numeric characters only',
-      },
-    });
+    return res
+      .status(400)
+      .json(
+        errMsg(
+          'username field cannot be blank and must contain alpha numeric characters only'
+        )
+      );
 
   if (!isUniqueUsername(username))
-    return res.status(400).json({
-      error: {
-        message: 'username is already registered',
-      },
-    });
+    return res.status(400).json(errMsg('username is already registered'));
 
   const validEmail = /^\w+@\w+\.?\w*$/;
   if (!email || !validEmail.test(email))
-    return res.status(400).json({
-      error: {
-        message: 'email field cannot be blank and must be a valid email',
-      },
-    });
+    return res
+      .status(400)
+      .json(errMsg('email field cannot be blank and must be a valid email'));
 
   if (!isUniqueEmail(email))
-    return res.status(400).json({
-      error: {
-        message: 'Email is already registered',
-      },
-    });
+    return res.status(400).json(errMsg('Email is already registered'));
   // Create user
-  const saltRounds = 10;
-  await encryptPassword(password, saltRounds)
-    .then((hash) => {
-      const newUser = { email, username, id: uuidv4(), ...extras };
-      users.push({ ...newUser, hash });
-      return createJwtToken(newUser, SECRET_JWT_KEY, {
-        algorithm: 'HS256',
-      }).then((jwt) => res.status(200).json({ ...newUser, jwt }));
-    })
-    .catch((err) => {
-      console.log('encryptPassword', err);
-      return res.status(500).json('Internal Server Error');
+  try {
+    const saltRounds = 10;
+    const hash = await encryptPassword(password, saltRounds);
+
+    const newUser = { email, username, id: uuidv4(), ...extras };
+    users.push({ ...newUser, hash });
+
+    const jwt = await createJwtToken(newUser, SECRET_JWT_KEY, {
+      algorithm: 'HS256',
     });
+
+    return res.json({ ...newUser, jwt });
+  } catch (err) {
+    console.log('encryptPassword', err);
+    return res.status(500).json('Internal Server Error');
+  }
 });
 
 router.post('/api/sessions', async (req, res) => {
   const { username, email, password } = req.body;
   if (username && email)
-    return res.status(400).json({
-      error: { message: 'Only send password with email OR username' },
-    });
+    return res
+      .status(400)
+      .json(errMsg('Only send password with email OR username'));
 
   const { hash, ...user } = username
     ? getUserByKeyValue({ username })
@@ -96,42 +95,38 @@ router.post('/api/sessions', async (req, res) => {
     ? getUserByKeyValue({ email })
     : null;
 
-  if (!user || !hash)
-    return res.status(400).json({ error: { message: 'User not found' } });
+  if (!user || !hash) return res.status(400).json(errMsg('User not found'));
 
-  verifyPassword(password, hash)
-    .then((isValidPassword) => {
-      if (!isValidPassword)
-        return res.status(400).json({ error: { message: 'User not found' } });
-      return createJwtToken(user, SECRET_JWT_KEY, { algorithm: 'HS256' }).then(
-        (jwt) => {
-          return res.status(200).json({ ...user, jwt });
-        }
-      );
-    })
-    .catch((err) => {
-      console.log('encryptPassword', err);
-      return res.status(500).json('Internal Server Error');
+  try {
+    if (!(await verifyPassword(password, hash)))
+      return res.status(400).json(errMsg('User not found'));
+
+    const jwt = await createJwtToken(user, SECRET_JWT_KEY, {
+      algorithm: 'HS256',
     });
+
+    return res.json({ ...user, jwt });
+  } catch (error) {
+    console.log('encryptPassword', err);
+    return res.status(500).json('Internal Server Error');
+  }
 });
 
 router.get('/api/sessions', async (req, res) => {
   const jwt = (req.headers.authorization || '').split(' ').pop();
-  if (!jwt)
-    return res.status(400).json({ error: { message: 'Not logged in' } });
+  if (!jwt) return res.status(400).json(errMsg('Not logged in'));
 
   const expireTime = Math.floor(Date.now() / 1000) - 60 * 60; // Token expires after 1hr
-  await verifyJwtToken(jwt, SECRET_JWT_KEY)
-    .then((decoded) => {
-      const { iat, ...payload } = decoded || {};
-      payload && iat > expireTime
-        ? res.status(200).json({ ...payload, jwt })
-        : res.status(400).json({ error: { message: 'Not logged in' } });
-    })
-    .catch((err) => {
-      console.log('verifyJwtToken', err);
-      return res.status(500).json('Internal Server Error');
-    });
+  try {
+    const { iat, ...payload } = await verifyJwtToken(jwt, SECRET_JWT_KEY);
+
+    if (payload && iat > expireTime) return res.json({ ...payload, jwt });
+
+    return res.status(400).json(errMsg('Not logged in'));
+  } catch (err) {
+    console.log('verifyJwtToken', err);
+    return res.status(500).json('Internal Server Error');
+  }
 });
 
 module.exports = router;
